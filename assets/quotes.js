397efda5307bef2d55367af61d4409c31919aa74
dwinurcahyo <66015798+dwinurcahyo@users.noreply.github.com>
2020-05-28 21:59:35 +0700

    Add files via upload

---
+++ assets/quotes.js
@@ -0,0 +1,10 @@
+// prettier-ignore
+export default [
+    { author: "Dwi Nurcahyo", quotes: "Whatever happens, keep breathing" },
+    { author: "Dwi Nurcahyo", quotes: "Kuliah yang bener jangan tipsen terus" },
+    { author: "Dwi Nurcahyo", quotes: "You're my laughter and the reason people ask me why i'm smiling when i don't even realize i am" },
+    { author: "Dwi Nurcahyo", quotes: "Jaga kesehatan ya" },
+    { author: "Dwi Nurcahyo", quotes: "I realized i was thinking of you , and i began to wonder how long you will been on my mind . Then it occurred to me : Since I met you , you,ve never left" },
+    { author: "Dwi Nurcahyo", quotes: "Sukses terus ya" },
+    { author: "Dwi Nurcahyo", quotes: "Salam buat orang tua lo hehe" },
+];
---
+++ assets/variables.js
@@ -0,0 +1,10 @@
+export default {
+  nicknames: ["Firstaap" , "Asa" , "Asang"],
+  greetings: {
+    evening: "Good Evening",
+    afternoon: "Good Afternoon",
+    day: "Have A Nice Day",
+    morning: "Good Morning",
+    night: "Good Night"
+  }
+};
